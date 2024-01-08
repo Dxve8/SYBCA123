fc4e2bda706ccc17cba23db43b733cdeb145e29e
Dave Goes <goesdave.12@gmail.com>
2024-01-08 10:52:25 +0530

    commit a branch

--- sybcaproject/src/sybcaproject/demo1.java
+++ sybcaproject/src/sybcaproject/demo1.java
@@ -3,8 +3,11 @@ package sybcaproject;
 public class demo1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello boi");
+		System.out.println("Hello WorlD");
 
+	}
+	void display () {
+	  System.out.println("branch commit ");
 	}
 
 }
